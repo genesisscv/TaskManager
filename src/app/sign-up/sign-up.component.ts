19f3176da14fa82fb8e59dd4451b2d9aa8aa998f
genesisscv <genesisscv@gmail.com>
2022-10-20 17:46:14 +1000

    updating sign up form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { CountriesService } from '../countries.service';
 import { Country } from '../country';
 
@@ -13,24 +13,76 @@ export class SignUpComponent implements OnInit {
   genders = ['male', 'female', 'non-binary'];
   countries: Country[] = [];
 
-  constructor(private countriesService: CountriesService) {}
+  constructor(
+    private countriesService: CountriesService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.countries = this.countriesService.getCountries();
 
-    this.signUpForm = new FormGroup({
-      firstName: new FormControl(null),
-      lastName: new FormControl(null),
-      email: new FormControl(null),
-      mobile: new FormControl(null),
-      dateOfBirth: new FormControl(null),
-      gender: new FormControl(null),
-      countryID: new FormControl(null),
-      receiveNewsLetters: new FormControl(null),
+    this.signUpForm = this.formBuilder.group({
+      personName: this.formBuilder.group({
+        firstName: null,
+        lastName: null,
+      }),
+
+      email: null,
+      mobile: null,
+      dateOfBirth: null,
+      gender: null,
+      countryID: null,
+      receiveNewsLetters: null,
+      skills: this.formBuilder.array([]),
     });
 
     this.signUpForm.valueChanges.subscribe((value: any) => {
-      console.log(value);
+      // console.log(value);
     });
+  }
+  onSubmitClick() {
+    //Display current form value
+    //console.log(this.signUpForm.value);
+
+    //setValue
+    // this.signUpForm.setValue({
+    //   firstName: "Adam",
+    //   lastName: "Smith",
+    //   email: "smith@gmail.com",
+    //   mobile: "9876543210",
+    //   dateOfBirth: "2020-01-01",
+    //   gender: "male",
+    //   countryID: 3,
+    //   receiveNewsLetters: true
+    // });
+
+    //patchValue
+    // this.signUpForm.patchValue({
+    //   firstName: "Adam",
+    //   lastName: "Smith",
+    //   email: "smith@gmail.com"
+    // });
+
+    //reset
+    this.signUpForm.reset();
+
+    //reset with Parameters
+    this.signUpForm.reset({
+      firstName: 'Adam',
+      lastName: 'Smith',
+      email: 'smith@gmail.com',
+    });
+  }
+
+  onAddSkill() {
+    var formGroup = new FormGroup({
+      skillName: new FormControl(null),
+      level: new FormControl(null),
+    });
+    (<FormArray>this.signUpForm.get('skills')).push(formGroup);
+  }
+
+  onRemoveClick(index: number) {
+    (<FormArray>this.signUpForm.get('skills')).removeAt(index);
   }
 }
